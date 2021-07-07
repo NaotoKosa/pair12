9d062e403db3b3a0258fa4833252ca5d1da4f528
id <id@IN0013390>
2021-07-07 16:42:33 +0900

    ---------------- 2021/07/07
    修正内容：
    会議室選択（roomlist.html）から
    座席表（seat.html）へ遷移
    
    
    ----------------

--- src/main/java/com/example/demo/RoomController.java
+++ src/main/java/com/example/demo/RoomController.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -31,17 +33,29 @@ public class RoomController {
 	}
 
 	/**
-	 * 情報入力画面を表示
+	 * 座席選択画面を表示
 	 */
 	@RequestMapping(value="/info")
-	public ModelAndView info(ModelAndView mv) {
+	public ModelAndView info(
+			@RequestParam("CODE") Integer code,
+			ModelAndView mv
+	) {
+		Optional<Room> room = roomRepository.findById(code);
 
-		mv.setViewName("info");
+		if (!room.isEmpty()) {
+			Room r = room.get();
+			Integer roomcode = r.getCode();
+
+			session.setAttribute("roomcode", roomcode);
+		}
+
+
+		mv.setViewName("seat");
 		return mv;
 	}
 
 	/**
-	 * 座席選択画面を表示
+	 *
 	 */
 	@RequestMapping(value="/seat")
 	public ModelAndView seat(ModelAndView mv) {
