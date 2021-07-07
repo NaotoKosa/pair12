914260da45446b692c531442515d3f7d17983c14
id <id@IN0013410>
2021-07-07 10:58:40 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/demo/Pair12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pair12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pair12Application.class, args);
+	}
+
+}
