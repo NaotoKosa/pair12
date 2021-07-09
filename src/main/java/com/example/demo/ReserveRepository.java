602c565872dd7ea4a30d30fd79139df2d220c94c
id <id@IN0013410>
2021-07-09 09:22:57 +0900

    修正内容：ユーザごとの予約履歴が表示されるようにしました。

--- src/main/java/com/example/demo/ReserveRepository.java
+++ src/main/java/com/example/demo/ReserveRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReserveRepository extends JpaRepository<Reserve, Integer>{
 
+	List<Reserve> findByUserscode(int userscode);
 }
